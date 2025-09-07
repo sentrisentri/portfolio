436f3527e31aebe7c53eb6883f1bd9dfcfdfee03
sentrisentri <kirubel.mulat@gmail.com>
2025-09-07 17:49:26 +0100

    Add Vercel Analytics and Speed Insights to enhance performance tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +31,8 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
